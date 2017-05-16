df3158c66d47313fb348b3e27f39f7940b4e61a5
Sea <s1511536@s.tsukuba.ac.jp>
2017-05-16 23:05:22 +0900

    signup.rb: input function for user_id & user_email, exeption rescue

--- signup.rb
+++ signup.rb
@@ -3,19 +3,36 @@
 require 'sqlite3'
 require 'pp'
 
-#ユーザーのサインアップ情報を受け取る(名前とメールアドレス、パスワード)仮の状態
-#IDはこちらで決定する
-user_id = 1
-user_mail = "a@example.com"
-user_pass = "passward"
-user_name = "A"
-
 include SQLite3
 
 db = Database.new("travel")
 
-sql = "insert into user_info values(#{user_id}, '#{user_name}', '#{user_pass}', '#{user_mail}');"
 
-db.execute(sql)
+#ユーザーのサインアップ情報を受け取る(名前とメールアドレス、パスワード)仮の状態
+#ユーザーにIDを入力してもらい、それをデータベースで確認し、ダブりがなければそのまま登録する
+print("ユーザーIDを入力してください:")
+user_id = gets.chomp
+print("メールアドレスを入力してください:")
+user_mail = gets.chomp
+user_pass = "passward"
+user_name = "A"
+
+
+sql = "insert into user_info values('#{user_id}', '#{user_name}', '#{user_pass}', '#{user_mail}');"
+
+begin
+
+  db.execute(sql)
+
+rescue => error
+  #matchメソッドはマッチしているときにブロックを実行する
+  #ブロック変数はマッチ部分が入り、配列指定して1番目を指定するとグループ部分のみを指定可
+  print("エラーが発生しました\n")
+  error.to_s.match(/user_info\.(\w+)$/){ |md|
+    print("この", md[1], "は既に使われています。\n")
+  }
+end
 
 db.execute("select * from user_info;") { |row| pp row}
+
+db.close
